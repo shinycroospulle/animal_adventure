947f3f013d12d3c9d4af90eea8c564f2d7fb7b7a
Shiny Croospulle <scroos01@students.poly.edu>
2014-04-08 15:56:54 -0400

    animal adventure

---
+++ Assets/Nemo1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nemo1 : MonoBehaviour {
+	string currentRoom = "underthesea";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string textBuffer = "";
+		if (currentRoom == "underthesea") {
+			textBuffer = "Finding Nemo adventure!";
+			currentRoom = "first";
+		}
+		else if (currentRoom == "first") {
+			textBuffer = "Encounters a shark! \nPress [A] to attack. \nPress [B] to ask for help. ";
+			if (Input.GetKeyDown (KeyCode.A) ) {
+				currentRoom = "attack";
+			}
+			if (Input.GetKeyDown (KeyCode.B) ) {
+				currentRoom = "help";
+			}
+		}
+		
+		else if (currentRoom == "attack") {
+			textBuffer = "Not strong enough, you are dead, can’t find nemo.";
+		}
+		
+		else if (currentRoom == "help") {
+			textBuffer = "The shark will give you directions, you can find nemo! ";
+			
+		}
+		
+		GetComponent <TextMesh> ().text = textBuffer;
+	}
+}
